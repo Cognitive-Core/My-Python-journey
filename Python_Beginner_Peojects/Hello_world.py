be6a6bf347f9cbfc7e50043c4c94826aebc564fe
cognitive-core <tejashvibhaskar@gmail.com>
2025-10-09 17:48:29 +0530

    end parameter of print

--- Python_Beginner_Peojects/Hello_world.py
+++ Python_Beginner_Peojects/Hello_world.py
@@ -11,4 +11,6 @@ print("hello world", 6, 7)
 
 print("hello world", 6, 7, sep="~")
 
+print("hello world", end="000\n")
+
 print("hello everyone, \"The Happy Prince\" was written by \"Oscar Wilde\"\nand the poem \"Fire and Ice\" is written by \"Robert Frost\"")

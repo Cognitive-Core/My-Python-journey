4c04682d0c39ae9997d02b52cc754d8252152fca
cognitive-core <tejashvibhaskar@gmail.com>
2025-10-09 17:36:31 +0530

    Enhance Hello_world.py with additional comments and examples for clarity

--- Python_Beginner_Peojects/Hello_world.py
+++ Python_Beginner_Peojects/Hello_world.py
@@ -1,2 +1,14 @@
+# This is the introduction to escape sequences, comments and print function
+
+# The pound symbol is used to write comments and ''' or """ can also be used to write comments
+
+'''Hello this is an example'''
+"""This is also an example"""
+
 print("hello world!")
-# hello
+
+print("hello world", 6, 7)
+
+print("hello world", 6, 7, sep="~")
+
+print("hello everyone, \"The Happy Prince\" was written by \"Oscar Wilde\"\nand the poem \"Fire and Ice\" is written by \"Robert Frost\"")

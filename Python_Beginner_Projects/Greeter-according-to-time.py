b04ab3ae3cfcbe57b255a7cbdc53b202903669a1
cognitive-core <tejashvibhaskar@gmail.com>
2025-10-18 19:04:22 +0530

    commit

--- Python_Beginner_Projects/Greeter-according-to-time.py
+++ Python_Beginner_Projects/Greeter-according-to-time.py
@@ -2,6 +2,8 @@ import time
 
 current_time = time.strftime("%H:%M:%S")
 
+print("current time is :", current_time)
+
 hour = int(time.strftime("%H"))
 
 minute = int(time.strftime("%M"))
@@ -12,7 +14,7 @@ if hour < 12 :
     print("Good Morning")
 elif hour >= 12 and hour < 18 :
     print("Good Afternoon") 
-elif hour == 18 and minute >= 1 and minute <= 59:
+elif hour >= 18 and hour < 21:
     print("Good evening")
 else :
     print("Good night")
